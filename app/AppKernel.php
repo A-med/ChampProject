72451e33fe2b217198a22804cdb68d92d52f5053
A-med <ahmeddammak91@gmail.com>
2016-05-26 01:41:35 +0100

    secondeCommit

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,6 +27,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
+        //saaaaaa
 
         return $bundles;
     }
